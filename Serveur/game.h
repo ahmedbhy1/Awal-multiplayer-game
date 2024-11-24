92eaa810ae4cf74c07d21c3f66568ba63b1a2be3
ahmed <ahmedbhyde@gmail.com>
2024-11-24 17:49:58 +0100

    add observer

--- Serveur/game.h
+++ Serveur/game.h
@@ -7,4 +7,8 @@
 int initiateGame(Client player1,Client player2,char* playerName);
 bool playGameTurn(Client player,int indexOfPlayer, int indexOfGame,int choosenDigit,char* opponentName);
 void showGameTable(int indexOfGame,char* nextPlayerName);
+void showCurrentGames(Client client);
+bool isValidGameIndexToJoinAsOb(int index);
+bool joinClientAsObserver(Client client,int indexOfGame);
+
 #endif /* guard */
